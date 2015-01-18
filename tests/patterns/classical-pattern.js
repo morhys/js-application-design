e8ae05d1f464bc99a3acd3bc08ed842363629f9c
morhys <morhys.m@googlemail.com>
2015-01-18 23:48:04 +0000

    widget

--- tests/patterns/classical-pattern.js
+++ tests/patterns/classical-pattern.js
@@ -17,18 +17,19 @@ function Bar(who) {
 Bar.prototype = Object.create(Foo.prototype);
 
 Bar.prototype.speak = function() {
-	console.log( "Hello, " + this.identify() + "." );
+	return "Hello, " + this.identify() + ".";
 };
 
 var b1 = new Bar("b1");
 var b2 = new Bar("b2");
 
-console.log(b1);
-console.log(b2)
-
 b1.speak();
-b2.speak();
 
+test('Classical Pattern', function(){
+
+	equal(b1.speak(), 'Hello, I am b1.', 'output works.')
+	equal(b2.speak(), 'Hello, I am b2.', 'output works.')
+});
 
 // OLOO PATTERN SAME FUNCTIONALITY AS ABOVE
 
@@ -45,17 +46,20 @@ var Foo2 = {
 var Bar2 = Object.create( Foo2 );
 
 Bar2.speak = function() {
-	console.log( "Hello, " + this.identify() + "." );
+	return "Hello, " + this.identify() + ".";
 };
 
 var a1 = Object.create( Bar2 );
-a1.init(" a1 ");
+a1.init("a1");
 
 var a2 = Object.create( Bar2 );
-a2.init(" a2 ");
+a2.init("a2");
 
-console.log(a1);
-console.log(a2)
+test('OLOO Pattern', function(){
+
+	equal(a1.speak(), 'Hello, I am a1.', 'output works.')
+	equal(a2.speak(), 'Hello, I am a2.', 'output works.')
+});
 
 
 
